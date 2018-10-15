2ce58daf33f403fd08517f70296c796e58f18936
huruikagi <huruikagi@gmail.com>
2018-10-15 19:07:33 +0900

    change out dir name

--- docs/add-to-home.e4057ff9.js
+++ docs/add-to-home.e4057ff9.js
@@ -106,8 +106,6 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"src/add-to-home.ts":[function(require,module,exports) {
 "use strict";
-
-window.alert("script works.");
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -135,7 +133,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62452" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57787" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
